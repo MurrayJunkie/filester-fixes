8887a5bf909568b1ef44b6eb419ec1e3c51c6664
MurrayJunkie <148269135+MurrayJunkie@users.noreply.github.com>
2026-03-30 14:23:22 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Filester-Fixes
+
+> A userscript that adds small quality-of-life improvements to Filester.  
+> I have some other problems with the website that I might have an attempt at fixing but they are not as easy as the currently offered solutions.  
+> Tested on Firefox. It should work on any browser that supports Tampermonkey.
+
+---
+
+## Currently Offered Features
+
+- Brings back the good old links to the album items. So you can open them in a new url or in the same page. HOWEVER you prefer to.
+- Inserts a referrer link to downloads to allow IDM to know the origin of the download for example.
+
+
+---
+
+## Usage
+
+Are finding the design choices of the website annoying?  
+This attempts to fix some of those design choices to fit my preferances.  
+
+---
+
+## Install
+
+### 1. Install Tampermonkey
+
+Install it from your browser’s extension store.
+
+### 2. Install the script
+
+- Open the [user.js](./user.js) file.  
+- Click the **Raw** button.  
+- Tampermonkey should prompt you to install it.  
+- If it doesn’t, create a new userscript manually and paste the contents of `user.js`.
+
+### 3. Done
+
+Website should allow you to navigate it a bit easier now.
---
+++ user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name         Filester-Fixes
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Some patches to fix the website behaviour that I find annoying.
+// @author       Murray
+// @match        https://filester.me/*
+// @match        https://filester.sh/*
+// @match        https://filester.si/*
+// @match        https://filester.gg/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=filester.me
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function downloadWithFullReferrer(target) {
+        const a = document.createElement("a");
+        a.href = target;
+        a.target = "_blank";
+        a.referrerPolicy = "unsafe-url"
+        a.download = "";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+
+    // Patch window.open to insert a referrer url to downloads.
+    ;((windowOpen) => {
+        unsafeWindow.open = function (url, target, features) {
+            if (target === "_blank" && url.endsWith("?download=true")) {
+                downloadWithFullReferrer(url)
+                return
+            }
+
+            return windowOpen.apply(this, arguments);
+        }
+    })(unsafeWindow.open)
+
+    // Insert links to album items instead of opening all in the current tab.
+    for (const item of document.querySelectorAll(".file-item")) {
+        const [ _, slug ] = /window\.location\.href='(.*?)'/.exec(item.getAttribute('onclick'))
+
+        item.removeAttribute("onclick");
+        const a = document.createElement("a");
+        a.href = slug;
+
+        Object.assign(a.style, {
+            position: "absolute",
+            inset: 0,
+            zIndex: 10,
+            textDecoration: "none",
+            color: "inherit"
+        });
+
+        if (getComputedStyle(item).position === "static") {
+            item.style.position = "relative";
+        }
+
+        item.appendChild(a);
+    }
+
+    // Your code here...
+})();
